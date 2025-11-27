ab95a8e78086da711ef42212d0fdaaa9a634b969
Arun Shivhare <arunshivhare925@gmail.com>
2025-11-27 11:18:28 +0530

    play songs

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ async function getsongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            // let song = element.href.split("/%5Csongs%5C")[1]
+            songs.push(element.href.split("/%5Csongs%5C")[1])
         }
     }
 
@@ -22,9 +23,28 @@ async function main() {
     let songs = await getsongs()
     console.log(songs)
 
+    let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songul.innerHTML = songul.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Arun</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+                            </li>`;
+    }
+
     // play the first song 
-    var audio = new Audio(songs[0]); 
-    audio.play();
+    var audio = new Audio(songs[0]);
+    // audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+
+    })
 }
 
 main()
